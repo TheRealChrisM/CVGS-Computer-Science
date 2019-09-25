0e477f9206b9bcb8cd9c0dd9f091ccac00a621a9
Chris Marotta <marottachristopher@gmail.com>
2019-09-25 14:27:02 -0400

    work on complexnumbers class from class

--- ComplexNumbers.py
+++ ComplexNumbers.py
@@ -10,11 +10,36 @@ class ComplexNumbers:
     def getImaginary(self):
         return self.__imaginary
 
+    def setReal(self, newReal):
+        self.__real = newReal
+
+    def setImaginary(self, newImaginary):
+        self.__imaginary = newImaginary
+
+    def abs(self):
+        a = self.getReal()
+        b = self.getImaginary()
+        c = (a**2 + b**2)**.5
+        return c
+    
     def __str__(self):
-        returnStatement = str(self.__real) + " + " + str(self.__imaginary) + "i"
+        returnStatement = str(self.getReal()) + " + " + str(self.getImaginary()) + "i"
         return returnStatement
 
     def __add__(self, other):
         realNums = self.getReal() + other.getReal()
         imaginaryNums = self.getImaginary() + other.getImaginary()
         return(ComplexNumbers(realNums, imaginaryNums))
+
+    def __sub__(self, other):
+        realNums = self.getReal() - other.getReal()
+        imaginaryNums = self.getImaginary() - other.getImaginary()
+        return(ComplexNumbers(realNums, imaginaryNums))
+
+    def __mul__(self, other):
+        realNums = (self.getReal()*other.getReal()) - (self.getImaginary()*other.getImaginary())
+        imaginaryNums = (self.getReal()*other.getImaginary())+(other.getReal()*self.getImaginary())
+        return(ComplexNumbers(realNums,imaginaryNums))
+
+    def __truediv__(self,other):
+        
